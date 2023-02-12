69cdd773ec08c759e9049bbdb75b8c70085cf4de
Rodrigo Bosarreyes <RodrigoBosarreyes@outlook.es>
2023-02-12 18:24:09 +0100

    xdd

--- .svelte-kit/generated/client/nodes/4.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/streamers/+page.svelte";
--- .svelte-kit/types/src/routes/streamers/proxy+page.server.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import type { PageServerLoad } from "./$types";
-
-export const load = async ({url}: Parameters<PageServerLoad>[0]) => {
-  const response = await fetch(url.origin + '/api/streamers');
-  const data = await response.json();
-  return data;
-};
