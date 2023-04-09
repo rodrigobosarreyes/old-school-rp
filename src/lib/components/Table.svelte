4a64d470f6e4393bb2cdf026849544291f2a2ab4
Rodrigo Bosarreyes <RodrigoBosarreyes@outlook.es>
2023-04-09 15:10:25 +0200

    Sort arrow

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import type { TableColumn } from "./TableColumn";
+	import { Icon } from "sveltestrap";
+  import type { TableColumn } from "./TableColumn";
 
   export let columns: TableColumn[];
   export let rows: any[];
@@ -15,6 +16,8 @@
     } else {
       return;
     }
+
+    columns = [...columns];
   
     const sortFunc = (a: any, b: any) => {
       const critA = a[col.prop];
@@ -54,7 +57,11 @@
               style:minWidth={col.minWidth}
               style:width={col.width}
               style:maxWidth={col.maxWidth}
-              class:sortable={col.sortOrder} on:click={() => sort(col)}>{col.name}</th>
+              class:sortable={col.sortOrder} on:click={() => sort(col)}>{col.name}
+              {#if col.sortOrder}
+                <Icon name={col.sortOrder === 'asc' ? 'caret-down-fill' : 'caret-up-fill'} />
+              {/if}
+            </th>
           {/each}
         </tr>
       </thead>
